229e7b53d77b93d912fb37253deefbc466598b46
Tigran Khachakranc <tigrankhachakranc@users.noreply.github.com>
2023-12-09 11:30:58 +0400

    Comments

--- Shapes/Rectangle.cpp
+++ Shapes/Rectangle.cpp
@@ -11,23 +11,24 @@ Rectangle::Rectangle(OptionsValues opt) : _opt(opt)
 
 void Rectangle::draw(QPainter &painter) const
 {
-    painter.setPen(getStyledPen(_opt));
+    painter.setPen(getStyledPen(_opt)); // [TK] Code duplication
     painter.drawRect(rect);
-    painter.setPen(Qt::NoPen);
+    painter.setPen(Qt::NoPen); // [TK] Code duplication
 }
 
 bool Rectangle::contains(const QPoint &point) const
 {
-    return rect.contains(point);
+    return rect.contains(point); // [TK] Code duplication
 }
 
 void Rectangle::move(const QPoint &offset)
 {
-    rect.moveTo(offset);
+    rect.moveTo(offset); // [TK] Code duplication
 }
 
 void Rectangle::resize(const QPoint &handlePos, const QPoint &resizeHandlePos)
 {
+     // [TK] Code duplication
     int deltaX = handlePos.x() - resizeHandlePos.x();
     int deltaY = handlePos.y() - resizeHandlePos.y();
     rect.adjust(0, 0, deltaX, deltaY);
@@ -35,11 +36,12 @@ void Rectangle::resize(const QPoint &handlePos, const QPoint &resizeHandlePos)
 
 QRect Rectangle::getRect() const
 {
-    return rect;
+    return rect; // [TK] Code duplication
 }
 
 bool Rectangle::isOnResizeHandle(const QPoint &pos) const
 {
+     // [TK] Code duplication
     int handleSize = 2;
     QRect handleRect = rect.adjusted(-handleSize, -handleSize, handleSize, handleSize);
     return handleRect.contains(pos);
@@ -47,5 +49,6 @@ bool Rectangle::isOnResizeHandle(const QPoint &pos) const
 
 QPen Rectangle::getStyledPen(const OptionsValues &_opt) const
 {
+     // [TK] Code duplication
     return ShapeBase::getStyledPen(_opt);
 }
