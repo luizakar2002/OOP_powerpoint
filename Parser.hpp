ce9026f6143fca80ad9a1e09ab38ccfc4c7e93f8
Tigran Khachakranc <tigrankhachakranc@users.noreply.github.com>
2023-12-09 12:24:18 +0400

    Comments

--- Parser.hpp
+++ Parser.hpp
@@ -14,12 +14,16 @@ using InputVector = std::vector<std::string>;
 using OptionsValues = std::unordered_map<std::string, std::string>;
 using CommandRegistry = std::unordered_map<std::string, Command*>;
 
+// TK: Too poorly organized parsing
+// instead of istream, parser gets vector which means tokenizer imitation outside of the parser
 class Parser
 {
 private:
+    // TK: you do onot need such members in the parser
     std::string     _main_command;
     InputVector     _command_line;
     OptionsValues   _options_values_map;
+    // TK: Command registry doesn't belong to the parser, parser should get it from outside
     CommandRegistry _command_registry;
 
 public:
@@ -31,6 +35,7 @@ public:
 
 public:
     Command    *parse(InputVector);
+    // Such public  methods are breaking Invariance of th parser 
     void        parse_main_command();
     void        parse_operands();
 
